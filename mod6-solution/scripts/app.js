1ed7ea3504f5758ecbd4ddb1e3859075bdf84e8d
Pipolo, Michael A <michael.pipolo@jhuapl.edu>
2022-03-08 20:30:35 -0500

    Fix up one minor thing.

--- mod6-solution/scripts/app.js
+++ mod6-solution/scripts/app.js
@@ -14,18 +14,16 @@
         $scope.checkForTooMuch = function () {
             if ($scope.items) {
                 $scope.messageClass = "good";
+
                 var splitItems = $scope.items.split(",");
                 var splitItemsWithoutEmpties = splitItems.filter(i => i.trim().length > 0);
-                if (splitItemsWithoutEmpties.length <= 3) {
-                    $scope.message = "Enjoy!";
-                    if (splitItemsWithoutEmpties.length < splitItems.length) {
-                        $scope.emptyItemsCaveat = "(Empty items [e.g., ', ,'] are not counted.)";
-                    } else {
-                        $scope.emptyItemsCaveat = "";
-                    }
-                } else {
-                    $scope.message = "Too much!";
-                }
+
+                $scope.emptyItemsCaveat = splitItemsWithoutEmpties.length < splitItems.length
+                    ? "(Empty items [e.g., ', ,'] are not counted.)"
+                    : "";
+                $scope.message = splitItemsWithoutEmpties.length <= 3
+                    ? "Enjoy!"
+                    : "Too much!";
             } else {
                 $scope.messageClass = "warning";
                 $scope.message = "Please enter data first";
