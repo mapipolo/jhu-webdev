d11206cb0c63fb5a6579bf8711b8c6f1f606e570
Pipolo, Michael A <michael.pipolo@jhuapl.edu>
2022-03-08 20:18:15 -0500

    Complete bonus portions.

--- mod6-solution/scripts/app.js
+++ mod6-solution/scripts/app.js
@@ -8,36 +8,27 @@
 
     function LunchCheckController($scope) {
         $scope.message = "";
+        $scope.messageClass = "hidden";
         $scope.emptyItemsCaveat = "";
 
-        $scope.getMessage = function () {
-            if ($scope.items == "") {
-                return "";
-            } else {
-                var splitItems = $scope.items.split(",");
-                if (splitItems.length <= 3)
-                    return splitItems;
-                else {
-                    return "dga;sd";
-                }
-            }
-        };
-        
         $scope.checkForTooMuch = function () {
-            if ($scope.items == "") {
-                $scope.message = "Please enter data first";
-            } else {
+            if ($scope.items) {
+                $scope.messageClass = "good";
                 var splitItems = $scope.items.split(",");
                 var splitItemsWithoutEmpties = splitItems.filter(i => i.trim().length > 0);
                 if (splitItemsWithoutEmpties.length <= 3) {
                     $scope.message = "Enjoy!";
-                    if (splitItemsWithoutEmpties.length < splitItems.length)
+                    if (splitItemsWithoutEmpties.length < splitItems.length) {
                         $scope.emptyItemsCaveat = "(Empty items [e.g., ', ,'] are not counted.)";
-                    else
+                    } else {
                         $scope.emptyItemsCaveat = "";
+                    }
                 } else {
                     $scope.message = "Too much!";
                 }
+            } else {
+                $scope.messageClass = "warning";
+                $scope.message = "Please enter data first";
             }
         };
     }
